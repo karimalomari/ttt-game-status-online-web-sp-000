56b9f534df7b63e10ffc883f2506397d8bfcf7a9
Karim Alomari <karim.alomari@gmail.com>
2020-07-14 11:54:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,5 +25,5 @@ def won?(board)
     postion_2 = board[win_index_2]
     postion_3 = board[win_index_3]
     
-    postion_1 == postion_2 && postion_2 == postion_3 && position_taken (board, win)
+    postion_1 == postion_2 && postion_2 == postion_3 && position_taken (board, win_index_1)
 end
